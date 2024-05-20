148d75a430ee5631f1151ea842392425ab36e1a2
Tushar kaushik <Tushar kaushik>
2024-05-21 00:43:29 +0530

    changes

--- middleware.js
+++ middleware.js
@@ -1,3 +1,9 @@
+export default function middleware(req, res, next) {
+    // Your middleware logic goes here
+    console.log("Middleware executed!");
+    // next(); // Call next() to pass control to the next middleware or route handler
+  }
+
 export const config = {
     matcher: ["/work-orders-create", "/work-orders"],
   };
